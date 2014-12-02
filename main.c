ae725c5292c124fb9602842e80ea4ef3dc7e16dc
gregtaole <le@et.esiea.fr>
2014-12-02 08:56:26 +0100

    Started to think of a way to implement double-buffering

--- main.c
+++ main.c
@@ -19,16 +19,21 @@ void init_image(int image[]) {
 }
 
 int main() {
-	int image[MAXSIZE], width, height;
+	int image1[MAXSIZE], image2[MAXSIZE], width, height;
 	width = 722;
 	height = 541;
 	int i=0, num=10, j, lol=0, temp;
 	int max=(width-1)*(height-1);
-	init_image(image);
+	int *p_image = image1;
+	FILE* out;
+
+	out=fopen("out.txt", "w");
+	init_image(image1);
+	//init_image(image2);
 	//image[height/3*width+width/4] = colorRGB(255, 255, 255);
 	mini_open("foobar", width, height);
 	printf("press ESC to quit\n");
-	//open_ppm(image);
+
 	while(i<num)
 	{
 		for(j=0; j<height; j++)
@@ -36,23 +41,24 @@ int main() {
 			temp=i;
 			for(lol=0; lol<width; lol+=temp*(width-1)/num)
 			{
-				drawPixel(lol, j, colorRGB(255, 0, 0), image, width, max);
-				usleep(50);
+				fprintf(out,"%d, %d\n", lol, temp);
+				drawPixel(lol, j, colorRGB(255, 0, 0), p_image, width, max);
 				temp++;
 			}
-			
+			mini_update(p_image);
 		}
-		//starField(image, max);
+		starField(p_image, max);
 		//drawLine(i*(width-1)/num, 0, i*(width-1)/num, height-1, colorRGB(i*(width-1)/num,i*(width-1)/num,i*(width-1)/num), image, width, max);	
-		mini_update(image);
+		
 		i++;
 	}
 	
 
 
 	while(0==0)
-		mini_update(image);
+		mini_update(p_image);
 
+	fclose(out);
 	mini_close();
 	return 0;
 }
