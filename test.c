1c6a0efd83dc39bf9876ed28d47ac91de6928889
Squadella <pbouniol@et.esiea.fr>
2014-12-25 16:30:05 +0100

    Starting the intro of the demo

--- test.c
+++ test.c
@@ -22,14 +22,15 @@ int main()
 {
 	//Initialisation of all the variables
 	srand(time(NULL));
-	int width, height/*, nbrLine, space, i*/;
+	int width, height/*, nbrLine, space*/, i, j, resize;
 	//Dynamic allocation of the variables to avoid segfault
 	int *image1 = malloc(sizeof(int)*MAXSIZE);
 	width = 800;
 	height = 600;
 	int max=(width)*(height);
 	init_image(image1);
-	uint palette[256];
+
+	//uint palette[256];
 	Pixel pixel3;
 
 	//Opening the image window
@@ -38,21 +39,34 @@ int main()
 
 	//Starting drawing!
 	
+	//The word demo is asceding the windows and become smaller
 	pixel3.x=100;
-	pixel3.y=100;
-	pixel3.color=colorRGB(255,0,0);
-	//
-	drawAlphabet('D', 10, image1, pixel3, width, max);
-	pixel3.x+=100;
-	pixel3.y+=10;
-	drawAlphabet('A', 10, image1, pixel3, width, max);
-	pixel3.x+=100;
-	pixel3.y-=10;
-	drawAlphabet('E', 10, image1, pixel3, width, max);
-	pixel3.x+=100;
-	drawAlphabet('M', 10, image1, pixel3, width, max);
-	pixel3.x+=110;
-	drawAlphabet('O', 10, image1, pixel3, width, max);
+	pixel3.y=height;
+	resize=10;
+	for(i=1; i<height; i++)
+	{
+		for(j=resize*6; j<width; j+=71*resize)
+		{
+			pixel3.color=colorRGB(i,i/3,i/2);
+			drawAlphabet('D', resize, image1, pixel3, width, max);
+			pixel3.x+=10*resize;
+			drawAlphabet('E', resize, image1, pixel3, width, max);
+			pixel3.x+=10*resize;
+			drawAlphabet('M', resize, image1, pixel3, width, max);
+			pixel3.x+=10*resize;
+			drawAlphabet('O', resize, image1, pixel3, width, max);
+			pixel3.x=j;
+		}
+		pixel3.y--;
+		if (i%(height/10)==0)
+		{
+			resize--;
+		}
+		mini_update(image1);
+		usleep(100);
+		fillImage(image1, colorRGB(0, 0, 0), width, max);
+	}
+	
 	//drawCircle2(pixel3, 100, image1, height, max);
 	//drawCircle(pixel3, 100, image1, height, width);
 
