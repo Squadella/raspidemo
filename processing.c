f4f66d767a5c550930de7ecfa533447310abfa0f
Squadella <pbouniol@et.esiea.fr>
2015-05-24 12:16:27 +0200

    *3

--- processing.c
+++ processing.c
@@ -801,14 +801,14 @@ void drawPlasma(char *image1, char *image2, char palette[], int max, int height,
 	fillImage(image1, 0, width, max);
 	while(loop)
 	{
-		for (i = 0 ; i < width; i++)
+		for (i = 0 ; i < width*3; i++)
 		{
 			for (j = 0; j < height; j++)
 			{
 				image1[j * width + i] = abs((int)(64 +(loop + 63 * sin( cos( i/(31*(j+1)) ))*cos(j*i+42)))% 256);
 			}
 		}
-		for (i = 0 ; i < width; i++)
+		for (i = 0 ; i < width*3; i++)
 		{
 			for (j = 0; j < height; j++)
 			{
