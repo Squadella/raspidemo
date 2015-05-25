6eda9ccc9796dc47d78b4ca89afca87e968b7993
Squadella <pbouniol@et.esiea.fr>
2015-05-25 18:29:34 +0200

    defrize

--- main.c
+++ main.c
@@ -89,7 +89,7 @@ void draw() {
         randRectangle(pixel5, pixel6, pixel7, pixel8, fbp, vinfo.xres, max);
         randRectangle(pixel9, pixel10, pixel11, pixel12, fbp, vinfo.xres, max);
         randRectangle(pixel13, pixel14, pixel15, pixel16, fbp, vinfo.xres, max);
-    	fillImage(fbp, 0, width, max);
+    	fillImage(fbp, 0, vinfo.xres, max);
     }
     sleep(1);
     starField(fbp, max, colorRGB(255,255,255), 100);
