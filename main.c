40d748bcdd8a8aac8fabc4017cf71d52fd655455
Squadella <pbouniol@et.esiea.fr>
2015-05-25 18:21:11 +0200

    epic toast

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ struct fb_fix_screeninfo finfo;
 void draw() {
 
     int x, y, max, i, lenghtRectangle, heightRectangle, random;
-	Pixel pixel1, pixel2, pixel3, pixel4;
+	Pixel pixel1, pixel2, pixel3, pixel4, pixel5, pixel6, pixel7, pixel8, pixel9, pixel10, pixel11, pixel12, pixel13, pixel14, pixel15, pixel16;
     max=vinfo.xres*vinfo.yres*3;
     char *fbp2 = malloc(max*sizeof(char));
     char *palette = malloc((256*3)*sizeof(char));
@@ -39,26 +39,56 @@ void draw() {
     pixel4.x = pixel1.x;
     pixel4.y = pixel1.y+heightRectangle;
 
+    pixel5.x = rand()%160;
+    pixel5.y = rand()%120;
+    pixel6.x = rand()%160;
+    pixel6.y = rand()%120;
+    pixel7.x = rand()%160;
+    pixel7.y = rand()%120;
+    pixel8.x = rand()%160;
+    pixel8.y = rand()%120;
+    pixel9.x = rand()%160;
+    pixel9.y = rand()%120;
+    pixel10.x = rand()%160;
+    pixel10.y = rand()%120;
+    pixel11.x = rand()%160;
+    pixel11.y = rand()%120;
+    pixel12.x = rand()%160;
+    pixel12.y = rand()%120;
+    pixel13.x = rand()%160;
+    pixel13.y = rand()%120;
+    pixel14.x = rand()%160;
+    pixel14.y = rand()%120;
+    pixel15.x = rand()%160;
+    pixel15.y = rand()%120;
+    pixel16.x = rand()%160;
+    pixel16.y = rand()%120;
+
 	pixel1.color = colorRGB(34,139,34);
 	pixel2.color = colorRGB(34,139,34);
     pixel3.color = colorRGB(34,139,34);
     pixel4.color = colorRGB(34,139,34);
+    pixel5.color = rand()%colorRGB(255,255,255);
+    pixel6.color = rand()%colorRGB(255,255,255);
+    pixel7.color = rand()%colorRGB(255,255,255);
+    pixel8.color = rand()%colorRGB(255,255,255);
+    pixel9.color = rand()%colorRGB(255,255,255);
+    pixel10.color = rand()%colorRGB(255,255,255);
+    pixel11.color = rand()%colorRGB(255,255,255);
+    pixel12.color = rand()%colorRGB(255,255,255);
+    pixel13.color = rand()%colorRGB(255,255,255);
+    pixel14.color = rand()%colorRGB(255,255,255);
+    pixel15.color = rand()%colorRGB(255,255,255);
+    pixel16.color = rand()%colorRGB(255,255,255);
 
     fillImage(fbp, 0, vinfo.xres, max);
     random=rand()%1000+500;
     for(i=0; i<random; i++)
     {
-
-        drawLine(pixel1, pixel2, fbp, vinfo.xres, max);
-        drawLine(pixel2, pixel3, fbp, vinfo.xres, max);
-        drawLine(pixel3, pixel4, fbp, vinfo.xres, max);
-        drawLine(pixel4, pixel1, fbp, vinfo.xres, max);
-        rand()%2 ? pixel1.x++ : pixel1.x--;
-        rand()%2 ? pixel2.x++ : pixel2.x--;
-        rand()%2 ? pixel3.x++ : pixel3.x--;
-        rand()%2 ? pixel4.x++ : pixel4.x--;
-        usleep(1000);
-        fillImage(fbp, 0, vinfo.xres, max);
+        randRectangle(pixel1, pixel2, pixel3, pixel4, fbp, vinfo.x, max);
+        randRectangle(pixel5, pixel6, pixel7, pixel8, fbp, vinfo.x, max);
+        randRectangle(pixel9, pixel10, pixel11, pixel12, fbp, vinfo.x, max);
+        randRectangle(pixel13, pixel14, pixel15, pixel16, fbp, vinfo.x, max);
     }
     sleep(1);
     starField(fbp, max, colorRGB(255,255,255), 100);
